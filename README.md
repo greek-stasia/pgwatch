ceea5ccdf3fbb4eb32e0b99c32cdba59bf02b421
Noah Kritz <noahkritz@gmail.com>
2020-04-04 23:40:52 -0400

    Added hashing and iterates through each word in the message to check the hashed value against what we provide

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pgwatch
+
+> Check yo self before you wreck yo self!
+
+## [Check yo self](https://www.youtube.com/watch?v=bCY9L3Xidoo)
--- pgwatch.js
+++ pgwatch.js
@@ -1,29 +1,41 @@
-var Discord = require("discord.io");
-var logger = require("winston");
-var auth = require("./auth.json");
+const Discord = require("discord.io");
+const logger = require("winston");
+const auth = require("./auth.json");
+const crypto = require("./auth.json");
+
 
 var prohibited = [
-  "shit",
-  "fuck",
-  "damn",
-  "damm",
-  "nigga",
-  "niggas",
-  "bitch",
-  "bitches",
-  "ass"
+  "bff272e9d673fa941d0a1920551d01a695516140",
+  "38d0f91a99c57d189416439ce377ccdcd92639d0",
+  "2336f5729424d0c84d319e991b3648cafa2c3c5b",
+  "f8a17e958f70b7990df493f4a53a36a67ba9163c",
+  "48770dfd3d23f09e02c2c70a37039de833009894",
+  "5f8bdcc367be8da26a270c09d29412021ca29a6e",
+  "3b19ecd69b492a40e3061f17786b33c28f504239",
+  "c0049442a7ca6d3b3eae5bfc4439eb4fd9e52464",
+  "8c4947e96c7c9f770aa386582e32ce7ce1b96e69",
+  "c177922cb7715a94aa4758eb140e08bfce4c5a04",
+  "266f83d202fa3da4a075cea751b4b8d6a30da1a8",
+  "39f6f95327b31d796f8d305a29df43b1d585e3cf",
+  "35ed5406781ebfdf7161bbbb18e16cb9ad1f3be4",
+  "b1aa14315bdbc2bdb4db442c176b0819f2eae550",
+  "08bc5beda7a9157ef65f8d90a511c77c8bedefa4",
+  "d7eb2aa54ec8d25420a7e45089969f7bdd0f4a9e"
 ];
+
 // Configure logger settings
+logger.level = "debug";
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console(), {
   colorize: true
 });
-logger.level = "debug";
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
   token: auth.token,
   autorun: true
 });
+
 bot.on("ready", function(evt) {
   logger.info("Connected");
   logger.info("Logged in as: ");
@@ -31,14 +43,13 @@ bot.on("ready", function(evt) {
 });
 
 bot.on("message", function(user, userID, channelID, message, evt) {
-  // Our bot needs to know if it will execute a command
-  // It will listen for messages that will start with `!`
-  if (prohibited.indexOf(message.toLowerCase()) >= 0) {
-    //var args = message.substring(1).split(" ");
-    //var cmd = args[0];
-    bot.sendMessage({
-      to: channelID,
-      message: "Watch your mouth!"
-    });
+  var words = message.split(" ");
+  for (word in words) {
+	  if (prohibited.indexOf(crypto.createHash('sha1').update(word).digest('hex')) >= 0) {
+		bot.sendMessage({
+		  to: channelID,
+		  message: "Watch your mouth!"
+		});
+	  }
   }
 });
